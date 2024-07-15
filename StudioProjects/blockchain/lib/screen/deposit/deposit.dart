76c7064a8de88bb488f4763549b94b18ac2a101d
Atul Banoriya <rajakatul502@.com>
2024-07-15 13:42:10 +0530

    Add existing project files prior to the push to GitHub.

---
+++ StudioProjects/blockchain/lib/screen/deposit/deposit.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class DepositView extends StatefulWidget {
+  const DepositView({super.key});
+
+  @override
+  State<DepositView> createState() => _DepositViewState();
+}
+
+class _DepositViewState extends State<DepositView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
